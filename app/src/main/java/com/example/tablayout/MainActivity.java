44c79b5c046f8b070d9de5d68ba085cd7b4530d6
jorgesaimun <saifullahsaimun1505@gmail.com>
2024-05-23 16:07:17 +0600

    Scroll fragment left and right

---
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewPager);
+
+
+        viewPagerMessengerAdapter adapter = new viewPagerMessengerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
